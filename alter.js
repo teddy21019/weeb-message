de5a11a625885a5b54affde22fedf16772ba4534
Fallenzone <pingfallenzone@gmail.com>
2021-09-17 00:43:50 +0900

    make flow cleaner

--- alter.js
+++ alter.js
@@ -1,27 +1,43 @@
-function transform(){
+function transform() {
     let text = document.getElementById('orig').value;
+    const rateOfWww = 0.2;
+    const rateOfPostfix = 0.6;
+    const rateOfPrefix = 0.2;
+
     text_array = text.split('\n');
-    
+
     new_text = '';
     text_array.forEach(line => {
-        const have_www = randomBool(0.2);
-        const www_number = ( Math.floor ( Math.random() * 4 ) + 2 ) * have_www; // 最多7個，最少 2個
-        const www = 'w'.repeat(www_number); 
-
-        const have_postfix = randomBool(0.6);
-        const postfix_index = Math.floor( Math.random() * postfixes.length );
-        const postfix  = have_postfix ? ( '(' + postfixes[postfix_index]) : '' ;
-
-        const have_prefix = randomBool(0.2);
-        const prefix_index = Math.floor( Math.random() * prefixes.length );
-        const prefix = have_prefix? prefixes[prefix_index] : '' ;
+        const www = generateW();
+        const postfix = generatePostFix();
+        const prefix = generatePreFix();
 
         new_text += `${prefix} ${line} ${www} ${postfix} <br />`;
 
     });
 
-    function randomBool(p){
-        return ( Math.random() < p ) ? 1 : 0;
+
+    function generateW() {
+        const have_www = randomBool(rateOfWww);
+        const www_number = (Math.floor(Math.random() * 4) + 2) * have_www; // 最多7個，最少 2個
+        return 'w'.repeat(www_number);
+    }
+
+
+    function generatePostFix() {
+        const have_postfix = randomBool(rateOfPostfix);
+        const postfix_index = Math.floor(Math.random() * postfixes.length);
+        return have_postfix ? ('(' + postfixes[postfix_index]) : '';
+    }
+
+    function generatePreFix() {
+        const have_prefix = randomBool(rateOfPrefix);
+        const prefix_index = Math.floor(Math.random() * prefixes.length);
+        return have_prefix ? prefixes[prefix_index] : '';
+    }
+
+    function randomBool(p) {
+        return (Math.random() < p) ? 1 : 0;
     }
 
     document.getElementById('result').innerHTML = new_text;
