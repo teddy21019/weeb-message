dfed69c2a06599e84666a49048c898d009017245
Fallenzone <pingfallenzone@gmail.com>
2021-09-17 00:25:18 +0900

    code improve

--- alter.js
+++ alter.js
@@ -1,20 +1,20 @@
 function transform(){
-    let text = document.getElementById("orig").value;
+    let text = document.getElementById('orig').value;
     text_array = text.split('\n');
     
-    new_text = "";
+    new_text = '';
     text_array.forEach(line => {
-        let have_www = randomBool(0.2);
-        let www_number = ( Math.floor ( Math.random() * 4 ) + 2 ) * have_www; // 最多7個，最少 2個
-        let www = "w".repeat(www_number); 
+        const have_www = randomBool(0.2);
+        const www_number = ( Math.floor ( Math.random() * 4 ) + 2 ) * have_www; // 最多7個，最少 2個
+        const www = 'w'.repeat(www_number); 
 
-        let have_postfix = randomBool(0.6);
-        let postfix_index = Math.floor( Math.random() * postfixes.length );
-        let postfix  = have_postfix ? ( '(' + postfixes[postfix_index]) : '' ;
+        const have_postfix = randomBool(0.6);
+        const postfix_index = Math.floor( Math.random() * postfixes.length );
+        const postfix  = have_postfix ? ( '(' + postfixes[postfix_index]) : '' ;
 
-        let have_prefix = randomBool(0.2);
-        let prefix_index = Math.floor( Math.random() * prefixes.length );
-        let prefix = have_prefix? prefixes[prefix_index] : '' ;
+        const have_prefix = randomBool(0.2);
+        const prefix_index = Math.floor( Math.random() * prefixes.length );
+        const prefix = have_prefix? prefixes[prefix_index] : '' ;
 
         new_text += `${prefix} ${line} ${www} ${postfix} <br />`;
 
@@ -24,5 +24,5 @@ function transform(){
         return ( Math.random() < p ) ? 1 : 0;
     }
 
-    document.getElementById("result").innerHTML = new_text;
+    document.getElementById('result').innerHTML = new_text;
 }
