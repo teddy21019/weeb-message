218e0444535125359c725531f3fa837cc5a91e1f
Ted <teddychenster@gmail.com>
2021-09-18 00:15:16 +0800

    new postfix added

--- alter.js
+++ alter.js
@@ -4,15 +4,15 @@ function transform(){
     
     let new_text = "";
     text_array.forEach(line => {
-        let have_www = randomBool(0.2);
+        let have_www = randomBool(0.3);
         let www_number = ( Math.floor ( Math.random() * 4 ) + 2 ) * have_www; // 最多7個，最少 2個
         let www = "w".repeat(www_number); 
 
-        let have_postfix = randomBool(0.6);
+        let have_postfix = randomBool(0.5);
         let postfix_index = Math.floor( Math.random() * postfixes.length );
         let postfix  = have_postfix ? ( '(' + postfixes[postfix_index]) : '' ;
 
-        let have_prefix = randomBool(0.2);
+        let have_prefix = randomBool(0.5);
         let prefix_index = Math.floor( Math.random() * prefixes.length );
         let prefix = have_prefix? prefixes[prefix_index] : '' ;
 
